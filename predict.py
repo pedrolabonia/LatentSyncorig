233ea7a28ae9a11c9aaf6da2710bfdcf0a2c3400
Pedro Labonia <pedro@xenoumena.com>
2025-04-21 02:06:59 -0300

    changing cog

--- predict.py
+++ predict.py
@@ -41,7 +41,8 @@ class Predictor(BasePredictor):
         self,
         video: Path = Input(description="Input video", default=None),
         audio: Path = Input(description="Input audio to ", default=None),
-        guidance_scale: float = Input(description="Guidance scale", ge=1, le=2.5, default=1.5),
+        guidance_scale: float = Input(description="Guidance scale", ge=1, le=2.5, default=2),
+        inference_steps: int = Input(description="Inference steps", ge=1, le=50, default=20),
         seed: int = Input(description="Set to 0 for Random seed", default=0),
     ) -> Path:
         """Run a single prediction on the model"""
@@ -57,6 +58,6 @@ class Predictor(BasePredictor):
 
         # Run the following command:
         os.system(
-            f"python -m scripts.inference --unet_config_path {config_path} --inference_ckpt_path {ckpt_path} --guidance_scale {str(guidance_scale)} --video_path {video_path} --audio_path {audio_path} --video_out_path {output_path} --seed {seed}"
+            f"python -m scripts.inference --unet_config_path {config_path} --inference_ckpt_path {ckpt_path} --guidance_scale {str(guidance_scale)} --inference_steps {inference_steps} --video_path {video_path} --audio_path {audio_path} --video_out_path {output_path} --seed {seed}"
         )
         return Path(output_path)
